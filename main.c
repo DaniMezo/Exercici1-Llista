34ab7d809b1a41e591ee6d435f12e3ddfc4a9ddf
danielmezo <daniel.mezo@students.salle.url.edu>
2019-02-07 21:11:17 +0100

    feat: creacio del metode llenarLista, per tal de crear elements i inserir-los en la llista

--- main.c
+++ main.c
@@ -11,3 +11,13 @@ void mostrarLista(ListaBid *lista)
         listabidAvanza(lista);
     }
 }
+void llenarLista(ListaBid *lista)
+{
+    printf("Insercion de los elementos\n");
+    listabidIntroduceOrdenado(lista, 5);
+    listabidIntroduceOrdenado(lista, 55);
+    listabidIntroduceOrdenado(lista, 10);
+    listabidIntroduceOrdenado(lista, 21);
+    listabidIntroduceOrdenado(lista, 24);
+    listabidIntroduceOrdenado(lista, 8);
+}
